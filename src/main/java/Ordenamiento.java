19727ea6fbdabb76e01f143135b6a708a4c88781
fabian esneider chuscano sierra <est.fabian.chuscano@unimilitar.edu.co>
2026-04-07 09:55:27 -0500

    se creo una opcion en donde el usuario agrega el tamaño del arreglo

--- src/main/java/Ordenamiento.java
+++ src/main/java/Ordenamiento.java
@@ -1,9 +1,27 @@
+
+import java.util.Scanner;
+
 public class Ordenamiento {
     public static void main(String[] args) {
+        
+    Scanner sc = new Scanner(System.in);   
+    
+        System.out.println("ingrese el tamaño del arreglo");
+        int n;
+        n = sc.nextInt();
+        
+        int v[] = new int[n];       
+        
+        
+        
+        
+        
         int arr[] = {5, 2, 11, -8, 115, 56, -3};
         ordenar(arr);
         System.out.println(java.util.Arrays.toString(arr));
     }
+    
+    
     private static void ordenar(int v[]) {
         for (int i = 0; i < v.length -1; i++) {
             for (int j = i+1; j < v.length ; j++) {
